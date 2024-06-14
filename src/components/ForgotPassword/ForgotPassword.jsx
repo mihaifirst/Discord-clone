7043af3543e75822e619081dcfc93d913ad4c446
mihaifirst <varzaru.alexmihai@gmail.com>
2024-06-15 01:22:14 +0300

    forgotpassword

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./ForgotPassword.scss";
 
 const ForgotPassword = () => {
-  return <div>Forgot-Password</div>;
+  return (
+    <>
+      <div className="container flex">
+        <div className="login-container">
+          <span>Forgot your Password?</span>
+          <p>Please enter your Email or Phone number below</p>
+          <div className="form">
+            <span>
+              EMAIL OR PHONE NUMBER <span>*</span>
+            </span>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Forgot - Password;
+export default ForgotPassword;
