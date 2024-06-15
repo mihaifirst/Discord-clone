df9314f56a45590cfcf09fd68d07a4bc37109cfe
mihaifirst <varzaru.alexmihai@gmail.com>
2024-06-16 01:24:53 +0300

    finished MainPage component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.scss";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
+import MainPage from "./components/MainPage/MainPage";
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
       {/* <Register /> */}
       {/* <ForgotPassword /> */}
+      <MainPage />
     </>
   );
 }
